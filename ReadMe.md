ea8b722f4191e0ce0902ad8c3595c09a39a8c57d
Shannon <shannon@42lines.net>
2014-12-28 16:19:46 -0500

    initial

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+Place ascii files inside the /banner folder and have a random one selected using a random color whenever your application starts. 
+
+![sample random-banner](demo.jpg)
---
+++ banner.js
@@ -0,0 +1,85 @@
+var colors = require('colors');
+var lineReader = require('line-reader');
+var fs = require("fs");
+var p = 'banners/';
+var DEBUG = false;
+var itterations = 1;
+var itteration = 0;
+
+if (DEBUG && process.argv.length > 2)
+	console.log(process.argv);
+
+//Read banner files into array (async)
+var files = [];
+if (process.argv.length > 2)
+	itterations = process.argv[2];
+	
+doWork();
+
+function doWork() {
+	fs.readdir(p, function (err, files) {
+		if (err) {
+			throw err;
+		}
+
+		files.map(function (file) { });
+		if (DEBUG)
+			console.log("done counting files");
+		drawRandomBanner(files);
+		files = [];
+	});
+}
+
+
+function drawRandomBanner(files) {
+var banner_color = randomIntFromInterval(1,9);
+	if (DEBUG)
+		console.log("banner color: "+banner_color);
+
+	var file = files[randomIntFromInterval(0,files.length-1)];
+	if (DEBUG)
+		console.log(file);
+	console.log("");
+	console.log("");
+
+	lineReader.eachLine('banners/'+file, function(line, last) {
+		console.log(colorWrap(line,banner_color));
+		if(last){
+			console.log(colorWrap("         symesh://urn:btih:12a1611484e76ad7922f1ae2b2442478cffb4f8c",banner_color));
+			itteration ++;
+			if (DEBUG)
+				console.log("on itteration "+ itteration + " of "+itterations);
+			if (itteration != itterations) {
+				doWork();				
+			}
+		}
+	});		
+}
+
+function colorWrap(txt,color) {
+    switch (color) {
+      case 1:
+      return(txt.red);
+      case 2:
+      return(txt.green);
+      case 3:
+      return(txt.yellow);
+      case 4:
+      return(txt.blue);
+      case 5:
+      return(txt.magenta);
+      case 6:
+      return(txt.cyan);
+      case 7:
+      return(txt.white);
+      case 8:
+      return(txt.gray);
+      case 9:
+      return(txt.zebra);
+    }
+}
+
+function randomIntFromInterval(min,max)
+{
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
